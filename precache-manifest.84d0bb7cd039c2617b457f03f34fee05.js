35f9582bc867217c604d8c935b43c7366d3d95e5
Valentyn Rubliuk <vrubliuk@gmail.com>
2019-09-03 19:54:17 +0300

    Updates

---
+++ precache-manifest.84d0bb7cd039c2617b457f03f34fee05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "30854f36ab9f16477077",
+    "url": "/portfolio/static/js/main.30854f36.chunk.js"
+  },
+  {
+    "revision": "d81b4db3e4b18d02f9b6",
+    "url": "/portfolio/static/js/1.d81b4db3.chunk.js"
+  },
+  {
+    "revision": "30854f36ab9f16477077",
+    "url": "/portfolio/static/css/main.bd82d8e2.chunk.css"
+  },
+  {
+    "revision": "42d49a16daf1a870e953bace3fa7d236",
+    "url": "/portfolio/index.html"
+  }
+];
