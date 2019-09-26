ac5fef2926cebffcb85da3782efe1109d4527633
Valentyn Rubliuk <vrubliuk@gmail.com>
2019-09-26 21:32:17 +0300

    Finish the dropdown component

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -2,24 +2,31 @@ import React, { useState } from "react";
 import classes from "./Dropdown.module.scss";
 import PropTypes from "prop-types";
 import icons from "../../assets/icons/index";
+import all from "../../helpers/all";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import onClickOutside from "react-onclickoutside";
 
 const Dropdown = ({ className, style, options, value, changeHandler }) => {
   const [isOpen, setIsOpen] = useState(false);
-  
+
+  Dropdown.handleClickOutside = () => setIsOpen(false);
+
   return (
     <div className={`${classes.dropdown} ${className}`} style={style}>
       <div className={classes.valueWrapper} onClick={() => setIsOpen(!isOpen)}>
         <div className={classes.value}>{value} </div>
         <div className={classes.iconWrapper}>
-          <FontAwesomeIcon icon={icons["caretDown" ] } />
-          
+          <FontAwesomeIcon icon={icons[isOpen ? "caretUp" : "caretDown"]} />
         </div>
       </div>
       {isOpen && (
         <div className={classes.options}>
-          {options.map( (option, i) => (
-            <div className={`${classes.option} ${option.value === value ? classes.optionActive : ""}`} onClick={() => changeHandler(option.value)} key={i}>
+          {options.map((option, i) => (
+            <div
+              className={`${classes.option} ${option.value === value ? classes.optionActive : ""}`}
+              onClick={() => all(() => changeHandler(option.value), () => setIsOpen(false))}
+              key={i}
+            >
               {option.label}
             </div>
           ))}
@@ -32,10 +39,12 @@ const Dropdown = ({ className, style, options, value, changeHandler }) => {
 Dropdown.propTypes = {
   className: PropTypes.string,
   style: PropTypes.object,
-  options: PropTypes.arrayOf(PropTypes.shape({
-    value: PropTypes.string,
-    label: PropTypes.string
-  })),
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.string,
+      label: PropTypes.string
+    })
+  ),
   value: PropTypes.string,
   changeHandler: PropTypes.func
 };
@@ -48,4 +57,8 @@ Dropdown.defaultProps = {
   changeHandler: () => {}
 };
 
-export default Dropdown;
+const clickOutsideConfig = {
+  handleClickOutside: () => Dropdown.handleClickOutside
+};
+
+export default onClickOutside(Dropdown, clickOutsideConfig);
